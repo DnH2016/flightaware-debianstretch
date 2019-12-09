1e1adad4a201a2107118fe92574cbbe2339d9c59
root <root@svr1.daverollinson.com>
2019-12-10 07:45:17 +0800

    Added code to get dev branch of piaware builder if version is stretch

--- getflightaware.sh
+++ getflightaware.sh
@@ -1,6 +1,13 @@
 #!
 cd tmp
-git clone https://github.com/flightaware/piaware_builder
+if grep -q "buster" /etc/os-release
+
+then
+	echo "Clone --branch dev for Buster"
+ 	git clone --branch dev https://github.com/flightaware/piaware_builder
+else
+        git clone https://github.com/flightaware/piaware_builder
+fi
 cd piaware_builder
 ./sensible-build.sh stretch
 cd package-stretch
