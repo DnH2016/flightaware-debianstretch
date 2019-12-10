be174d9737258267a3d4fff975a2551af0a334d2
David Rollinson <davidrollinson@hotmail.com>
2019-12-10 09:51:11 +0800

    Fixed the package selection

--- buildflightaware.sh
+++ buildflightaware.sh
@@ -1,5 +1,10 @@
 #!
+if grep -q "buster" /etc/os-release
+then
+cd tmp/piaware_builder/package-buster
+else
 cd tmp/piaware_builder/package-stretch
+fi
 dpkg-buildpackage -b -uc -us
 cd ..
 dpkg -i piaware*.deb
